236b24fe81ef4009e58d53b900900469ea34299b
Damon German <damon.german@hyland.com>
2022-05-07 10:14:56 -0500

    made some changes

---
+++ intro-oop/Location.cs
@@ -0,0 +1,28 @@
+﻿namespace intro_oop
+{
+    public class Location
+    {
+        private double _x;
+        private double _y;
+
+        public double GetX()
+        {
+            return _x;
+        }
+
+        public double GetY()
+        {
+            return _y;
+        }
+
+        public void SetX(double x)
+        {
+            _x = x;
+        }
+
+        public void SetY(double y)
+        {
+            _y = y;
+        }
+    }
+}
