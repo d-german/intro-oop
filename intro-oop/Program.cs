28630640231fb6ca09b1ba33c16127de6c0ee0dc
Damon German <damon.german@hyland.com>
2022-06-25 14:23:34 -0500

    made some changes

--- intro-oop/Program.cs
+++ intro-oop/Program.cs
@@ -2,13 +2,8 @@
 
 using intro_oop;
 
-Console.WriteLine("Hello, World!");
-
 Console.WriteLine(new Circle(1.0).Area());
 
 var location1 = new Location { X = 1.0, Y = 2.0 };
 var location2 = new Location { X = 1.0, Y = 2.0 };
-
-var areEqual = location1 == location2;
-
-Console.WriteLine(areEqual);
+Console.WriteLine(location1.Equals(location2));
